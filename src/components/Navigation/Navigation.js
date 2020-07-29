b1736a608e1a1a79b02c8654a5d83ae2ebc2f76c
mixereqv1 <mateuszral7@gmail.com>
2020-07-29 23:21:49 +0200

    setting up project for work

---
+++ src/components/Navigation/Navigation.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const Navigation = () => (
+  <nav>
+    <span>HATTA WEB</span>
+    <ul>
+      <li>articles</li>
+      <li>about</li>
+      <li>gallery</li>
+      <li>contact</li>
+    </ul>
+  </nav>
+);
+
+export default Navigation;
---
+++ src/pages/gallery.js
@@ -0,0 +1,5 @@
+import React from 'react';
+
+const GalleryPage = () => <h1>Gallery page</h1>;
+
+export default GalleryPage;
